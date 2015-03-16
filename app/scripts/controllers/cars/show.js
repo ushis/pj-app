b27b9cda058b525ed151649140dc5c40b2b01f2a
ushi <ushi@honkgong.info>
2015-03-16 23:07:43 +0100

    Check for all possible child states

--- app/scripts/controllers/cars/show.js
+++ app/scripts/controllers/cars/show.js
@@ -2,9 +2,28 @@
 
 angular
   .module('pjApp')
-  .controller('CarsShowCtrl', function($scope, car, Car) {
+  .controller('CarsShowCtrl', function($scope, $state, car, Car) {
 
     $scope.car = car.car;
+
+    $scope.isActiveState = function(name) {
+      switch (name) {
+        case 'reservations':
+          return $state.includes('app.car.reservations') ||
+            $state.includes('app.car.reservation-new') ||
+            $state.includes('app.car.reservation');
+        case 'rides':
+          return $state.includes('app.car.rides') ||
+            $state.includes('app.car.ride-new') ||
+            $state.includes('app.car.ride');
+        case 'drivers':
+          return $state.includes('app.car.owners') ||
+            $state.includes('app.car.borrowers') ||
+            $state.includes('app.car.driver-new');
+        default:
+          return $state.includes('app.car.' + name);
+      }
+    };
 
     $scope.reloadCar = function() {
       Car.get({carId: $scope.car.id}).$promise
