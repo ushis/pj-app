876d8e4764635e232eddb9f8e383f51dae406ed7
ushi <ushi@honkgong.info>
2016-03-19 15:40:17 +0100

    group reservations

--- app/scripts/directives/calendar.js
+++ app/scripts/directives/calendar.js
@@ -4,10 +4,49 @@ angular
   .module('pjApp')
   .directive('calendar', function(_, moment) {
 
+    var Group = function(items) {
+      this.items = [];
+
+      if (!_.isUndefined(items)) {
+        this.add(items);
+      }
+    };
+
+    Group.prototype.add = function(items) {
+      if (_.isArray(items)) {
+        this.items = this.items.concat(items);
+      } else {
+        this.items.push(items);
+      }
+
+      this.items.sort(function(a, b) {
+        return a.startsAt.diff(b.startsAt);
+      });
+    };
+
+    Group.prototype.conflicts = function(item) {
+      var start = item.startsAt.clone().startOf('day');
+      var end = item.endsAt.clone().endOf('day');
+
+      return !this.items.every(function(i) {
+        return i.endsAt.clone().endOf('day').isBefore(start) ||
+          i.startsAt.clone().startOf('day').isAfter(end);
+      });
+    };
+
+    Group.prototype.first = function() {
+      return this.items[0];
+    };
+
+    Group.prototype.last = function() {
+      return this.items[this.items.length-1];
+    };
+
     var Week = function(moment) {
       this.nr = moment.week();
       this.start = moment.clone().startOf('week');
       this.end = moment.clone().endOf('week');
+      this.groups = [];
 
       this.days = _.range(7).map(function(day) {
         return this.start.clone().add(day, 'day');
@@ -44,6 +83,32 @@ angular
         return start.week() === this.nr;
       }
       return start.week() <= this.nr && this.nr <= end.week();
+    };
+
+    Week.prototype.setItems = function(items) {
+      var groups = [];
+
+      items
+        .sort(function(a, b) {
+          var aStart = a.startsAt.clone().startOf('day');
+          var aEnd = a.endsAt.clone().endOf('day');
+          var bStart = b.startsAt.clone().startOf('day');
+          var bEnd = b.endsAt.clone().endOf('day');
+          return bEnd.diff(bStart) - aEnd.diff(aStart);
+        })
+        .forEach(function(item) {
+          var group = groups.find(function(g) {
+            return !g.conflicts(item);
+          });
+
+          if (_.isUndefined(group)) {
+            groups.push(new Group(item));
+          } else {
+            group.add(item);
+          }
+        });
+
+      this.groups = groups;
     };
 
     var Calendar = function(date, updateCallback) {
@@ -87,6 +152,14 @@ angular
       return moment().startOf('day').isSame(day);
     };
 
+    Calendar.prototype.setItems = function(items) {
+      this.weeks.forEach(function(week) {
+        week.setItems(items.filter(function(item) {
+          return week.contains(item.startsAt, item.endsAt);
+        }));
+      });
+    };
+
     return {
       restrict: 'E',
       scope: {
@@ -97,6 +170,10 @@ angular
       templateUrl: 'views/directives/calendar.html',
       link: function(scope) {
         scope.calendar = new Calendar(new Date(), scope.change);
+
+        scope.$watch('items', function(items) {
+          scope.calendar.setItems(items);
+        });
 
         scope.onItemClick = function(item) {
           scope.click({$item: item});
@@ -111,12 +188,6 @@ angular
             parts.push(item.endsAt.format('HH:mm'));
           }
           return parts.join(' - ');
-        };
-
-        scope.itemsInWeek = function(week) {
-          return scope.items.filter(function(item) {
-            return week.contains(item.startsAt, item.endsAt);
-          });
         };
       }
     };
